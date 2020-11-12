4cb154c619aa216fad4f943b1d77b9f1b21cbb49
MaxFerrara <59260954+MaxFerrara@users.noreply.github.com>
2020-11-13 00:30:47 +0700

    Добавил 3е д/з

---
+++ HomeWork_3/HomeWork_3/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HomeWork_3
+//
+//  Created by Max on 02.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
